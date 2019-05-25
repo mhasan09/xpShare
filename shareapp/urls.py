68621cd50454c7241afb38fc84e1a552c13c6be4
Mahabubul Hasan <shourav6688@gmail.com>
2019-05-26 00:26:57 +0600

    added template customization

--- shareapp/urls.py
+++ shareapp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shareapp.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', getIndex, name='index'),
+    path('author/<name>', getProfile, name='author'),
+
 ]
